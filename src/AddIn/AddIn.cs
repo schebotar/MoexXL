9aed4c55eef855e4fcc5513ab821baab275825b7
Sergey Chebotar <s.chebotar@gmail.com>
2021-12-25 16:37:38 +0300

    Add ExcelDNA, Newtonsoft.JSON

---
+++ src/AddIn/AddIn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+
+namespace MoexXL
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoClose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AutoOpen()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
